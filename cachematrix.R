ad3507e7cac3593e6245cd735f35f82dfcdf17d3
hank222 <henry_0990@hotmail.com>
2020-07-23 17:26:09 -0500

    Update cachematrix.R
    Cache matrix comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,11 @@
 ## functions do
 
 ## Write a short comment describing this function
+##set Null to matrix
+##set the value of the matrix
+##get the value of the matrix
+##set the inverse of the matrix
+##get the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
@@ -18,7 +23,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+##gets the inverse from the cache and skips the computation. Otherwise, it calculates the iverse of the matrix 
+##and sets the inverse of the matrix in the cache via the setinverse function.
 cacheSolve <- function(x, ...) {
         m <- x$getinverse()
         if(!is.null(m)) {
